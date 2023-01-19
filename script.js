a426fa94337136a52891bcba5435fd2ef1e61243
PREETY CHAURASIYA <121533750+preety-chaurasiya@users.noreply.github.com>
2023-01-19 21:30:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function myfunction(){
+    var x =document.getElementById("pass");
+
+   if(x.type ==="password"){
+        x.type ="text";
+    }
+    else{
+        x.type ="password";
+    }
+}
+function validation(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+         if(password.value.length >= 8){
+            alert("Login Successfull");
+            window.location.replace("newpage.html");
+            return false;
+        }
+        else{
+            alert("login Failed");
+        }
+}
